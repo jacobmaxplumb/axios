b95571bfae54870cbea7657571cb9fde9270e381
jacobplumblambda <jacob.plumb@lambdaschool.com>
2021-09-08 19:57:22 -0400

    added functions to index.js file

--- project/index.js
+++ project/index.js
@@ -1,1 +1,24 @@
-console.log('hello');
+const { default: axios } = require("axios");
+
+const getPeople = () => {
+    axios.get('http://localhost:3000/people').then(res => {
+        console.log(res.data);
+    })
+}
+
+const getPersonById = (id) => {
+    axios.get(`http://localhost:3000/people/${id}`).then(res => {
+        console.log(res.data);
+    })
+}
+
+const addPerson = (person) => {
+    axios.post('http://localhost:3000/people', person).then(res => {
+        console.log(res);
+    })
+}
+
+//getPeople();
+//getPersonById(1);
+const p = {firstName: "James", lastName: "Plumb", age: 30};
+addPerson(p);
